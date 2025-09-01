1d8df1b3a7384765d42eeca075ff7dcb4fd05d8f
Samuel Rodriguez <dantesamuelrodriguez@gmail.com>
2025-08-31 19:42:24 -0500

    fix(auth): enable secure cookie settings based on environment
    Set proper cookie security flags (httpOnly, secure, sameSite) based on NODE_ENV to enhance security in production while maintaining functionality in development

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -28,9 +28,9 @@ export const register = async (req, res) => {
     const token = await createAccessToken({ id: newUser.id });
 
     res.cookie("token", token, {
-      // httpOnly: process.env.NODE_ENV === 'development', // descomesntar en producción
-      // secure: false, // true in production
-      // sameSite: 'none' // descomesntar en producción
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     });
 
     res.json({
@@ -63,9 +63,9 @@ export const login = async (req, res) => {
     const token = await createAccessToken({ id: user.id });
 
     res.cookie("token", token, {
-      // httpOnly: process.env.NODE_ENV === 'development',// descomesntar en producción
-      // secure: false, // true en producción
-      // sameSite: 'none' // descomesntar en producción
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     });
 
     res.json({
